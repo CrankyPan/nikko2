9f83f8083ab4d8c2ab2af2b8512f2de0297aa07b
MUHAMMAD YAZID <95423355+CrankyPan@users.noreply.github.com>
2024-12-15 01:06:08 +0800

    Update ViewPackageController.java

--- src/main/java/pack/controller/ViewPackageController.java
+++ src/main/java/pack/controller/ViewPackageController.java
@@ -42,46 +42,7 @@ public class ViewPackageController extends HttpServlet {
 	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String id = request.getParameter("id"); 
-
-        if (id != null) { 
-            try {
-                packageId = Integer.parseInt(id); 
-
-                //call getConnection() method 
-                con = ConnectionManager.getConnection();
-
-                //3. create statement
-                ps = con.prepareStatement("SELECT * FROM packages WHERE packageId=?");
-                ps.setInt(1, packageId);
-
-                //4. execute query
-                rs = ps.executeQuery();
-
-                if(rs.next()) {
-                    packageId = rs.getInt("packageId");
-                    packageName = rs.getString("packageName"); 
-                    packagePrice = rs.getDouble("packagePrice"); 
-                }
-
-                //5. close connection 
-                con.close(); 
-
-            } catch (NumberFormatException e) {
-                System.out.println("Error: Invalid package ID format."); 
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Error: No package ID provided."); 
-        }
-
-        request.setAttribute("packageId", packageId);
-        request.setAttribute("packageName", packageName); 
-        request.setAttribute("packagePrice", packagePrice); 
-
-        RequestDispatcher req = request.getRequestDispatcher("viewPackage.jsp"); 
-        req.forward(request, response);
+       
     }
 
 	/**
